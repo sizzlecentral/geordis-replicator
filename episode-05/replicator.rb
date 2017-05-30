e16ddb9108fba7d05ac9bc4f3c8ccda148d7cd04
Don Cooper <don_cooper@hotmail.com>
2017-05-30 18:09:16 -0400

    Episode 5 debugged

--- episode-05/replicator.rb
+++ episode-05/replicator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Replicator
 
   attr_reader :plate
@@ -74,7 +76,6 @@ class Replicator
   end
 
   def transport_glass_to_replicator_plate
-    return
     @enterprise.transporter.energize(obj: glass_in_tummy, from: @tummy, to: @plate)
   end
 
